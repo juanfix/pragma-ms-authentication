9100eb088be425cbdeaf6152695204869f13c418
Juan Jose Ceballos Navia <juanfix@gmail.com>
2025-09-10 15:23:57 -0500

    feat: se implementa la configuracion de Docker y de agregan migraciones con liquidbase

--- applications/app-service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO role (id, name, description) VALUES
- (1, 'ADMIN', 'Administrador del sistema'),
- (2, 'ASESOR', 'Asesor de la plataforma'),
- (2, 'CLIENTE', 'Cliente de la plataforma')
- ON CONFLICT (id) DO NOTHING;
